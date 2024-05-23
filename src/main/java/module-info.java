4baede4b5d9702d5e6a3e09e146410149018b979
lyhduc <lyhongduc.34@gmail.com>
2024-05-23 21:34:36 +0700

    Commit init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.hqtqlsv {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.hqtqlsv to javafx.fxml;
+    exports com.example.hqtqlsv;
+}
